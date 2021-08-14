6ddb1c2b3fa80d52eba2daa73060ff7662c1bc7e
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2021-08-14 16:59:15 +0530

    addStaff form all the validations completed.

--- childEdit.php
+++ childEdit.php
@@ -117,11 +117,6 @@ $gender = $row_pro['gender'];
 
                                 </div><!--form-group Ends-->
 
-
-                                
-
-
-
                                  <div class="form-group"><!--form-group starts-->
 
                                     <label class="col-md-3 control-label">Gender</label>
@@ -153,12 +148,6 @@ $gender = $row_pro['gender'];
                                 </div><!--form-group Ends-->
 
                                 <!--Dinindu Add image end-->
-                                
-                                 
-
-                                    
-                                
-                                
                                     
                                     <div class="form-group" ><!-- form-group Starts -->
 
@@ -197,63 +186,57 @@ $gender = $row_pro['gender'];
      $tmp_name = $_FILES['cimage']['tmp_name'];
      $error = $_FILES['cimage']['error'];
 
-     // name with initials format check
-    if (!preg_match("/^[a-zA-Z-' . ]*$/",$name_with_init)) {
-        echo "<script>alert('Only letters, Dots and white space allowed for Name with Initials.')</script>";
-        echo "<script> window.open('index.php?insertChild ','_self')</script>"; 
+        // name with initials format check
+        if (!preg_match("/^[a-zA-Z-' . ]*$/",$name_with_init)) {
+            echo "<script>alert('Only letters, Dots and white space allowed for Name with Initials.')</script>";
+            echo "<script> window.open('index.php?insertChild ','_self')</script>"; 
 
-    // full name format check
-    }elseif(!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-        echo "<script>alert('Only letters and white space allowed for Full Name')</script>";
-        echo "<script> window.open('index.php?insertChild ','_self')</script>";       
-    }
+        // full name format check
+        }elseif(!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            echo "<script>alert('Only letters and white space allowed for Full Name')</script>";
+            echo "<script> window.open('index.php?insertChild ','_self')</script>";       
+        }
 
-    else{
-        if($error === 0){
-            // maximum 3MB images
-           if($img_size > 1024*1024*3){
-               $em = "Sorry, image is too large";
-               
-           }else{
-               $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-               $img_ex_lc = strtolower($img_ex);
-   
-               $allowed_exs = array("jpg", "jpeg", "png");
-   
-               if(in_array($img_ex_lc, $allowed_exs)){
-                   $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
-                   $img_upload_path = 'uploads/'.$new_img_name;
-                   move_uploaded_file($tmp_name, $img_upload_path);
-   
-                  
-                   // Insert into database
-                   $insert_staff = "update childdetails  set name_with_init='$name_with_init', name='$name',birthdate='$bdate',gender='$gender', image_url='$new_img_name' where id= '$edit_id'";
-   
-                  $run_staff = mysqli_query($Con, $insert_staff);
-              
-                  if ($run_staff) {
-                      echo "<script> alert('child Added successfully ')</script>";
-                      echo "<script> window.open('index.php?viewChild ','_self')</script>";
-                  }
-   
-   
-               }else{
-                  $em = "You can't upload files of this type";
-               }
-           }
+        else{
+            if($error === 0){
+                // maximum 3MB images
+            if($img_size > 1024*1024*3){
+                $em = "Sorry, image is too large";
+                
+            }else{
+                $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                $img_ex_lc = strtolower($img_ex);
+    
+                $allowed_exs = array("jpg", "jpeg", "png");
+    
+                if(in_array($img_ex_lc, $allowed_exs)){
+                    $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+                    $img_upload_path = 'uploads/'.$new_img_name;
+                    move_uploaded_file($tmp_name, $img_upload_path);
+    
+                    
+                    // Insert into database
+                    $insert_staff = "update childdetails  set name_with_init='$name_with_init', name='$name',birthdate='$bdate',gender='$gender', image_url='$new_img_name' where id= '$edit_id'";
+    
+                    $run_staff = mysqli_query($Con, $insert_staff);
+                
+                    if ($run_staff) {
+                        echo "<script> alert('child Added successfully ')</script>";
+                        echo "<script> window.open('index.php?viewChild ','_self')</script>";
+                    }
+    
+    
+                }else{
+                    $em = "You can't upload files of this type";
+                }
+            }
 
-           echo "<script>alert('$em')</script>";
-       }else{
-          $em = "unknown error occurred!";
-       }
+            echo "<script>alert('$em')</script>";
+        }else{
+            $em = "unknown error occurred!";
+        }
 
-    }
-
-     ////////////////
-     
-
-
-     ////////////////
+        }
     
     }
     ?>
